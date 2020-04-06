72313b076ede23712468d2e590acc10e47aa379a
NIK <nikita7cb@gmail.com>
2020-04-06 17:00:22 +0200

    Questo è il primmo commit

---
+++ script0.js
@@ -0,0 +1,52 @@
+'use strict';
+
+
+var leftBorderWidth=1;
+let second = 2;
+const pi=3.14;
+
+console.log(leftBorderWidth);
+
+
+var number=5;
+var string= "hello";
+var sym=Symbol();
+var boolian=true;
+null ;
+undefined;
+var obj={};                         //strutture, oggetto 
+console.log(4/0);                   //Infinity
+console.log('string'*9);            //NaN
+
+let persone={
+    name:"john",
+    age:25,
+    isMarried:false
+}
+
+console.log(persone.name);            
+console.log(persone["name"]);
+
+let arr=['plum.png','orange.png','apple.png'];
+
+
+//alert("sciao");
+//let answer=confirm("sei sicuro ?");
+//let answer= prompt("Hai 18 anni?", "");             // tutte info recevute dall utente si hanno sotto forma di string 
+//console.log(typeof(answer));                        // string
+
+let n0= 10, 
+    n1=10;
+
+//console.log(++n0);
+//console.log(--n1);
+
+console.log(n0++);
+console.log(n1--);
+
+console.log(5%2);
+
+console.log("2"==2);                                   // retorna true 
+console.log("2"===2);                                  // restituisce false poche confronta il tipo
+
+
